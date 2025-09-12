f8c75ccbd6311caecfaf4b3cbc3ec31e915945c1
Kanha-01 <guptakanha2220@gmail.com>
2025-09-12 17:36:17 +0530

    storyController done @9:00

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,7 @@ import fs from 'fs'
 import imagekit from "../configs/imageKit.js"
 import Connection from "../models/Connection.js"
 import Post from "../models/post.js"
+import { inngest } from "../inngest/index.js"
 
 // Get User DAta using userId
 export const getUserData = async (req,res) => {
@@ -200,10 +201,16 @@ export const sendConnectionRequest = async (req,res) => {
     })
 
     if(!connection){
-      await Connection.create({
+      const newConnection = await Connection.create({
         from_user_id : userId,
         to_user_id : id
       })
+
+      await inngest.send({
+        name : 'app/connection-request',
+        data : { connectionId : newConnection._id }
+      })
+
       return res.json({success : true , message : 'Connection request sent successfully'})
     }
     else if(connection && connection.status === 'accepted'){
