648d6beb3aed08e5db393072f5d0baea6fe78aa2
Kanha-01 <guptakanha2220@gmail.com>
2025-09-13 12:41:47 +0530

    create Post and fetch feed done @10:28

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -4,46 +4,65 @@ import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import Postcard from '../components/Postcard'
 import RecentMesssages from '../components/RecentMesssages'
+import { useAuth } from '@clerk/clerk-react'
+import toast from 'react-hot-toast'
+import api from '../api/axios'
 
 const Feed = () => {
 
-const [feeds, setfeeds] = useState([])
-const [loading, setLoading] = useState(true)
+    const [feeds, setfeeds] = useState([])
+    const [loading, setLoading] = useState(true)
+    const { getToken } = useAuth()
 
-const fetchFeeds = async () => {
-    setfeeds(dummyPostsData)
-    setLoading(false)
-}
+    const fetchFeeds = async () => {
+        try {
+            setLoading(true)
+            const { data } = await api.get('/api/post/feed', {
+                headers:
+                    { Authorization: `Bearer ${await getToken()}` }
+            })
 
-useEffect(()=> {
-    fetchFeeds()
-},[])
+            if(data.success){
+                setfeeds(data.posts)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+        setLoading(false)
+    }
 
-  return !loading ? (
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
-        
-        {/* Stories and Post List */}
-        <div>
-            <StoriesBar />
-            <div className='p-4 space-y-6'>
-                {feeds.map((post)=>(
-                    <Postcard key={post._id} post={post}/>
-                ))}
+    useEffect(() => {
+        fetchFeeds()
+    }, [])
+
+    return !loading ? (
+        <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+
+            {/* Stories and Post List */}
+            <div>
+                <StoriesBar />
+                <div className='p-4 space-y-6'>
+                    {feeds.map((post) => (
+                        <Postcard key={post._id} post={post} />
+                    ))}
+                </div>
+            </div>
+
+            {/* Right SideBar */}
+            <div className='max-xl:hidden sticky top-0'>
+                <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+                    <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+                    <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+                    <p className='text-slate-600'>Email Marketing</p>
+                    <p className='text-slate-400'>Supercharge your market with a powerful, easy-to-use platform built for results</p>
+                </div>
+                <RecentMesssages />
             </div>
         </div>
-        
-        {/* Right SideBar */}
-        <div className='max-xl:hidden sticky top-0'>
-            <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
-                <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
-                <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
-                <p className='text-slate-600'>Email Marketing</p>
-                <p className='text-slate-400'>Supercharge your market with a powerful, easy-to-use platform built for results</p>
-            </div>
-            <RecentMesssages/>
-        </div>
-    </div>
-  ) : <Loading />
+    ) : <Loading />
 }
 
 export default Feed
